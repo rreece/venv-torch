9377a87410e1fb56b2e199eefec1a0c52fa44a46
Ryan Reece <rreece@tenstorrent.com>
2022-11-29 11:17:28 -0800

    cleanup setup

--- setup.sh
+++ setup.sh
@@ -10,22 +10,22 @@
 path_of_this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 path_above_this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
 
-add_to_python_path()
-{
-    export PYTHONPATH=$1:$PYTHONPATH
-    echo "  Added $1 to your PYTHONPATH."
-}
-
 add_to_path()
 {
-    export PATH=$1:$PATH
+    export PATH=$1:${PATH:+:${PATH}}
     echo "  Added $1 to your PATH."
 }
 
 add_to_ld_library_path()
 {
-    export LD_LIBRARY_PATH=$1:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=$1:${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
     echo "  Added $1 to your LD_LIBRARY_PATH."
+}
+
+add_to_python_path()
+{
+    export PYTHONPATH=$1:${PYTHONPATH:+:${PYTHONPATH}}
+    echo "  Added $1 to your PYTHONPATH."
 }
 
 
@@ -44,9 +44,11 @@ fi
 
 
 #-----------------------------------------------------------------------------
-# setup PYTHONPATH
+# setup paths
 #-----------------------------------------------------------------------------
 
-echo "  Setting up your PYTHONPATH."
+add_to_path /usr/local/cuda-11.8/bin
+add_to_ld_library_path /usr/local/cuda-11.8/lib64
 add_to_python_path ${path_of_this_dir}
-echo "  done."
+
+echo "  Done."
